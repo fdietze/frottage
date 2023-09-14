112207480ebabe86fefe183337fec6f07842199b
Felix Dietze <felix.dietze@rwth-aachen.de>
2023-09-15 01:16:18 +0200

    fix timeout (#5)
    * Update generate.ts
    
    * Update util.ts

--- util.ts
+++ util.ts
@@ -1,3 +1,13 @@
 export async function sleepMs(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export async function timeout<T>(fn: () => Promise<T>, ms: number): Promise<T> {
+    const timeout = new Promise<T>((_, reject) => {
+        setTimeout(() => {
+            reject(new Error('Operation timed out'));
+        }, ms);
+    });
+
+    return Promise.race([fn(), timeout]);
+}
